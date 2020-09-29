e430a72bcea36c7cb23c8ec81effa25f6efefc60
Dung Nguyen <dudu.nt107@gmail.com>
2020-09-29 16:03:48 -0500

    fixed some bugs

--- script.js
+++ script.js
@@ -39,84 +39,94 @@ questionBank = [
         choice3: "alerts",
         choice4: "numbers",
         answer: "alerts"
-    },   
+    },
 ]
 
 //Calling varibales here
 var choices = document.querySelector("#choices");
 var countDown = document.querySelector("#timer");
-var timeInterval;   
+var timeInterval;
 var currentQuestion = 0;
-var total = 75;
+var total = 40;
 
-//start the quiz here
-$(document).ready(function() {
-quizStart();
-function quizStart(){ 
-    timerInterval = setInterval(function(){
-    countDown.textContent = total + " seconds";
-    total--;
-    //if the timer reaches 0, execute the function endQuiz
-    if(total<=0){
-        endQuiz();
+$(document).ready(function () {
+    //start the quiz here
+    quizStart();
+    function quizStart() {
+        timerInterval = setInterval(function () {
+            countDown.textContent = total + " seconds";
+            total--;
+            //if the timer reaches 0, execute the function endQuiz
+            if (total <= 0) {
+                total = 0;
+                countDown.textContent = total;
+                endQuiz();
+            }
+        }, 1000);
+        //Otherwise, display next questions
+        setQuestion();
+    };
+
+    // Display questions - Execute function setQuestion
+    function setQuestion() {
+        $("#question").text(questionBank[currentQuestion].question);
+        $("#choice1").text(questionBank[currentQuestion].choice1);
+        $("#choice2").text(questionBank[currentQuestion].choice2);
+        $("#choice3").text(questionBank[currentQuestion].choice3);
+        $("#choice4").text(questionBank[currentQuestion].choice4);
     }
-},1000);
-    //Otherwise, display questions
-    setQuestion();
-};
 
-// Display questions - Execute function setQuestion
-function setQuestion(){
-    $("#question").text(questionBank[currentQuestion].question);
-    $("#choice1").text(questionBank[currentQuestion].choice1);
-    $("#choice2").text(questionBank[currentQuestion].choice2);
-    $("#choice3").text(questionBank[currentQuestion].choice3);
-    $("#choice4").text(questionBank[currentQuestion].choice4);
-}
-    
-// onclick event for the response  
-// verify if the choice is right or wrong 
-//incrase currentQuestion and verify if there is more, if yes go to setQuestion otherwise go to endQuiz
-    $(".choices").click(function(){
-        var response = $(this).attr("id")
+    // onclick event for the response  
+    // verify if the choice is right or wrong 
+    //incrase currentQuestion and verify if there is more, if yes go to setQuestion otherwise go to endQuiz
+    $(".choices").click(function () {
+        var response = $(this).attr("id");
+        var message =  $("#message");
         var choiceClicked = questionBank[currentQuestion][response]
-        if (choiceClicked === questionBank[currentQuestion].answer){
-            $("#message").html("<p class='right'>Right!</p>");
-        }else{
-            $("#message").html("<p class='wrong'>Wrong!</p>");
-            total-=10;
+        if (choiceClicked === questionBank[currentQuestion].answer) {
+           message.html("<p class='right'>Right!</p>");
+        } else {
+            message.html("<p class='wrong'>Wrong!</p>");
+            total -= 10;
+            countDown.textContent = total + " seconds";
         }
+        message.html ="";
         currentQuestion++;
-        if (currentQuestion < questionBank.length && total <0){
+        
+       
+        if (currentQuestion === questionBank.length) {
             endQuiz();
         }
-        else if(currentQuestion === questionBank.length){
-            endQuiz();
-        }
-        else{
+        else {
             setQuestion();
         }
-})
+    })
 
-// function endQuiz starts when time ran out or when all questions have been finished!
-// show the form and grab the initials and save in localStorage
-function endQuiz(){
-    clearInterval(timerInterval);
-    $(".container").html("<div class = 'end-result'></div>");
-    $(".end-result").append("<h3>All Done!</h3>");
-    $(".end-result").append("<p>Your final score is: " + total + "</p>");
-    $(".end-result").append("<div class ='initial-form'></div>");
-    $(".initial-form").append("<label>Enter your initial:</label>");
-    $(".initial-form").append("<input id ='initial'></input>");
-    $(".initial-form").append("<a href='highScore.html' id ='link'></a>");
-    $("#link").append("<button class = 'button' id='submit'>Submit</button>")
+    // function endQuiz starts when time ran out or when all questions have been finished!
+    // show the form and grab the initials and save in localStorage
+    function endQuiz() {
+        clearInterval(timerInterval);
+        if (total<=0){
+            total = 0;
+            countDown.textContent = total;
+        }
+        
+        $(".container").html("<div class = 'end-result'></div>");
+        $(".end-result").append("<h3>All Done!</h3>");
+        $(".end-result").append("<p>Your final score is: " + total + "</p>");
+        $(".end-result").append("<div class ='initial-form'></div>");
+        $(".initial-form").append("<label>Enter your initial:</label>");
+        $(".initial-form").append("<input id ='initial'></input>");
+        $(".initial-form").append("<a href='highScore.html' id ='link'></a>");
+        $("#link").append("<button class = 'button' id='submit'>Submit</button>")
     }
+    $("#submit").click(function () {
+        return window.location.assign('highscore.html')
+    })
 })
 
-$("#submit").click(function(){
-    return window.location.assign('highscore.html')
-})
-    
+
+
 
 
 
